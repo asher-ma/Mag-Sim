82ee63697e7b98ac4655dcdb476787b238e77e69
asher-ma <asherhmangel@gmail.com>
2026-09-06 00:43:37 -0700

    Made Charge attributes public

--- MagSim/Charge/Include/Charge.h
+++ MagSim/Charge/Include/Charge.h
@@ -3,7 +3,7 @@
 
 template <class ItemType>
 class Charge {
-    private :
+    public :
         int x; // x coordinate (m)
         int y; // y coordinate (m)
         int Q; // Measurment of charge in coulombs (C)
