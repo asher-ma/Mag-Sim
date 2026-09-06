b20050f631e2ceac871ecfcfa4879883de8a241c
asher-ma <asherhmangel@gmail.com>
2026-09-06 16:05:02 -0700

    Implements charge object along with electromagnetic calculation operations

--- MagSim/Include/Phys.h
+++ MagSim/Include/Phys.h
@@ -1,64 +1,40 @@
-#include <iostream>
-#include <string>
+#include "Geometry.h"
 #include <vector>
-#include <cmath>
 
-struct Vector3D {
-    double x{0}, y{0}, z{0}; // Coordinates (m)
-
-
-    // Vector properties
-    // Get magnitude of vector
-    double mag() {
-        return sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
-    };
-
-    // Get direction of vector
-    Vector3D unit() {
-        double m = mag();
-        if (m == 0) {
-            return Vector3D(0,0,0);
-        };
-        return *this/m;
-    };
-
-
-    // Math operators
-    // This vector + other vector 
-    Vector3D operator+(const Vector3D& o) const { return Vector3D(x + o.x, y + o.y, z + o.z); };
-
-    // This vector - other vector
-    Vector3D operator-(const Vector3D& o) const { return Vector3D(x - o.x, y - o.y, z - o.z); };
-
-    // This vector / scalar value
-    Vector3D operator/(const double& s) const {
-        if (s == 0) {
-            throw std::runtime_error("Vector divide by 0 error");
-        };
-        return Vector3D(x/s, y/s, z/s);
-    };
-
-    //  Print operator (prints coordinates)
-    friend std::ostream& operator<<(std::ostream& os, const Vector3D& v) {
-        os << "(" << v.x << "," << v.y << "," << v.z << ")";
-        return os;
-    };
+// Charge header
+class Charge {
+    public :
+        Vector3D r; // Position vector
+        double Q; // Measurment of charge in coulombs (C)
 };
 
+// Field header
+namespace Electromagnetism {
+    const double k = 1; // Coulomb's constant placeholder
 
-
-// Geometry namespace: includes geometric functions that act between objects
-// Geometry header
-namespace Geometry {
-    double dist(Vector3D r1, Vector3D r2); // Gets distance between two vectors
-    Vector3D dir(Vector3D r1, Vector3D r2); // Gets direction from v1 to v2
+    Vector3D Force(const Charge& q1, const Charge& q2);
+    Vector3D Force(const Charge& q1, const std::vector<Charge> charges);
+    double E; // Measurment of field (V/m or N/C)
 };
 
-// Geometry implementation
-double Geometry::dist(Vector3D r1, Vector3D r2) {
-    return sqrt(pow(r1.x - r2.x, 2) + pow(r1.y - r2.y, 2) + pow(r1.z - r2.z, 2));
-};
+// Field implementation
+Vector3D Electromagnetism::Force(const Charge& q1, const Charge& q2){
+    Vector3D ur12 = Geometry::dir(q2.r, q1.r); // Unit vector for direction from q2 to q1
+    double r = Geometry::dist(q1.r, q2.r); // Distance between charges
+    
+    // Coulombs law: k((q1*q2)/r2)r12
+    double FMag = k * (q1.Q * q2.Q) / pow(r, 2);
 
-Vector3D Geometry::dir(Vector3D r1, Vector3D r2) {
-    return r2 - r1;
-};
+    Vector3D F = ur12 * FMag;
+    
+    return F;
+}; 
+
+Vector3D Electromagnetism::Force(const Charge& q1, const std::vector<Charge> charges){
+    Vector3D F;
+    for (const Charge& q : charges) {
+        F = F + Force(q1, q);
+    };
+
+    return F;
+}; 
--- MagSim/main.cpp
+++ MagSim/main.cpp
@@ -2,37 +2,91 @@
 #include <string>
 
 #include "Geometry.h"
-
-using namespace std;
+#include "Phys.h"
 
 void displayVectorProperties(Vector3D r) {
-    cout << "r: " << r << "\nmag: " << r.mag() << "\nunit: " << r.unit() << endl;
+    std::cout << "r: " << r << "\nmag: " << r.mag() << "\nunit: " << r.unit() << std::endl;
 };
 void displayVectorRelations(Vector3D r1, Vector3D r2) {
-    cout << "r: " << r2-r1 << "\ndist: " << Geometry::dist(r1, r2)
-        << "\ndir: " << Geometry::dir(r1, r2) << endl;
+    std::cout << "r: " << r2-r1 << "\ndist: " << Geometry::dist(r1, r2)
+        << "\ndir: " << Geometry::dir(r1, r2) << std::endl;
 };
+double rad(double deg){
+    return deg * (M_PI/180);
+}
 
-int main() {
-    // Vector operations tests
+// Vector operations tests
+void VectorGeometryTests() {
+    std::cout << "\n-----------------------------------------------------------------------------\nVECTOR OPERATIONS\n";
     Vector3D r0(0, 0, 0);
     Vector3D r1(3, 4, 0);
     Vector3D r2(6, 8, 0);
     
-    cout << "Vector r0:\n";
+    std::cout << "Vector r0:\n";
     displayVectorProperties(r0);
-    cout << "\nVector r1:\n";
+    std::cout << "\nVector r1:\n";
     displayVectorProperties(r1);
-    cout << "\nVector r2:\n";
+    std::cout << "\nVector r2:\n";
     displayVectorProperties(r2);
     
     // Vector geometry tests
-    cout << "\nVector r01:\n";
+    std::cout << "\nVector r01:\n";
     displayVectorRelations(r0, r1);
-    cout << "\nVector r12:\n";
+    std::cout << "\nVector r12:\n";
     displayVectorRelations(r1, r2);
-    cout << "\nVector r02:\n";
+    std::cout << "\nVector r02:\n";
     displayVectorRelations(r0, r2);
+}
+
+// Electromagnetic operations tests
+void ElectromagneticTests() {
+    std::cout << "\n-----------------------------------------------------------------------------\nElectromagnetic force operations\n";
+    const int s = 1;
+    {
+        std::cout << "Test A:\n" << "Expected: (0,2,0)\n";
+        Charge q0(Vector3D(0,0,0), 1);
+        std::vector<Charge> charges = {
+            Charge(Vector3D(-s*sin(rad(60)), -s*cos(rad(60)), 0), 1),
+            Charge(Vector3D(0,-s,0), 1),
+            Charge(Vector3D(s*sin(rad(60)), -s*cos(rad(60)), 0), 1)
+        };
+        Vector3D F =  Electromagnetism::Force(q0, charges);
+        std::cout << "F(q0): " << F << std::endl
+                << "Mag: " << F.mag() << std::endl
+                << "Dir: " << F.unit() << std::endl;
+    }
+
+    {
+        std::cout << "\nTest B:\n" << "Expected: (0,3,0)\n";
+        Charge q0(Vector3D(0,0,0), 1);
+        Charge q1(Vector3D(0,-s,0), 3);
+        Vector3D F =  Electromagnetism::Force(q0, q1);
+        std::cout << "F(q0): " << F << std::endl
+                << "Mag: " << F.mag() << std::endl
+                << "Dir: " << F.unit() << std::endl;
+    }
+
+    {
+        std::cout << "\nTest C:\n" << "Expected: (-3sqrt(3)/2,3/2,0) OR (" << (-double(3) * sqrt(3))/2 << "," << double(3)/2 << ",0)\n";
+        Charge q0(Vector3D(0,0,0), 1);
+        std::vector<Charge> charges = {
+            Charge(Vector3D(-s*sin(rad(60)), -s*cos(rad(60)), 0), -3),
+            Charge(Vector3D(0,-s,0), 3)
+        };
+        Vector3D F =  Electromagnetism::Force(q0, charges);
+        std::cout << "F(q0): " << F << std::endl
+                << "Mag: " << F.mag() << std::endl
+                << "Dir: " << F.unit() << std::endl;
+    }
+
+    {
+        
+    }
+}
+
+int main() {
+    //VectorGeometryTests();    
+    ElectromagneticTests();
 
     return 0;
-};
+}
