4a246b453ee9b026906b91184bd83985218e02ea
asher-ma <asherhmangel@gmail.com>
2026-09-06 16:30:06 -0700

    Use field calculations to find force

--- MagSim/Include/Electromagnetism.h
+++ MagSim/Include/Electromagnetism.h
@@ -8,10 +8,10 @@
 namespace Electromagnetism {
     const double k = 1; // Coulomb's constant placeholder
 
-    Vector3D Force(const Charge& q1, const Charge& q2);
-    Vector3D Force(const Charge& q1, const std::vector<Charge> charges);
     Vector3D FieldAt(const Vector3D r, const Charge& q);
     Vector3D FieldAt(const Vector3D r, const std::vector<Charge> charges); // Measurment of field (V/m or N/C)
+    Vector3D Force(const Charge& q1, const Charge& q2);
+    Vector3D Force(const Charge& q1, const std::vector<Charge> charges);
 };
 
 #include "..\src\Electromagnetism.cpp"
--- MagSim/src/Electromagnetism.cpp
+++ MagSim/src/Electromagnetism.cpp
@@ -1,29 +1,8 @@
 #include "Electromagnetism.h"
 #include "Geometry.h"
 
-Vector3D Electromagnetism::Force(const Charge& q1, const Charge& q2){
-    Vector3D ur12 = Geometry::dir(q2.r, q1.r); // Unit vector for direction from q2 to q1
-    double r = Geometry::dist(q1.r, q2.r); // Distance between charges
-    
-    // Coulombs law: k((q1*q2)/r2)r12
-    double FMag = k * (q1.Q * q2.Q) / pow(r, 2);
-
-    Vector3D F = ur12 * FMag;
-    
-    return F;
-}; 
-
-Vector3D Electromagnetism::Force(const Charge& q1, const std::vector<Charge> charges){
-    Vector3D F;
-    for (const Charge& q : charges) {
-        F = F + Force(q1, q);
-    };
-
-    return F;
-}; 
 
 Vector3D Electromagnetism::FieldAt(const Vector3D r, const Charge& q) {
-    Vector3D E;
     Vector3D uqr = Geometry::dir(q.r, r); // Unit vector for direction from q to r
     double d = Geometry::dist(r, q.r); // Distance from q to r
 
@@ -40,4 +19,18 @@ Vector3D Electromagnetism::FieldAt(const Vector3D r, const std::vector<Charge> c
     };
 
     return E;
+};
+
+Vector3D Electromagnetism::Force(const Charge& q1, const Charge& q2){
+    Vector3D E = FieldAt(q1.r, q2);
+    Vector3D F = E * q1.Q;
+    
+    return F;
+}; 
+
+Vector3D Electromagnetism::Force(const Charge& q1, const std::vector<Charge> charges){
+    Vector3D E = FieldAt(q1.r, charges);
+    Vector3D F = E * q1.Q;
+
+    return F;
 };
