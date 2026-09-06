782878c7a94535d03623c8b0c6c6e2b380072af2
asher-ma <asherhmangel@gmail.com>
2026-09-06 00:38:21 -0700

    Created basic particle class

---
+++ MagSim/Particle/Include/Particle.h
@@ -0,0 +1,12 @@
+#ifndef _PARTICLE_
+#define _PARTICLE_
+
+template <class ItemType>
+class Particle {
+    private :
+        int x; // x coordinate
+        int y; // y coordinate
+        int Q; // Measurment of charge in coulombs (C)
+};
+
+#endif
