662d6793f77d41d0afe8b9c59c4e791abc9d3968
asher-ma <asherhmangel@gmail.com>
2026-09-18 16:09:07 -0700

    Adds Simulation class

---
+++ MagSim/Include/Simulation.h
@@ -0,0 +1,18 @@
+#ifndef _SIMULATION_
+#define _SIMULATION_
+
+#include <vector>
+
+class Simulation {
+    private: 
+        // vector<custom 3d objects> objects; // TODO
+        void getForces(); // TODO
+        void getCollisions(); // TODO
+        void moveObjects(); // TODO
+    public:
+
+};
+
+#include "..\src\Simulation.cpp"
+
+#endif // Simulation
